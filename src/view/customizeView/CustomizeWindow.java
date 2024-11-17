c4e46d763015978575758be237ca4a26873e9fd8
HARDY130 <haohardy.hu@mail.utoronto.ca>
2024-11-17 13:34:47 -0500

    SearchFood View 1.0

--- src/view/customizeView/CustomizeWindow.java
+++ src/view/customizeView/CustomizeWindow.java
@@ -1,5 +1,8 @@
 package view.customizeView;
 
+import view.customizeView.add_foods_view.AddFoodsLayout;
+import view.customizeView.add_foods_view.AddFoodsView;
+import view.customizeView.search_foods_view.SearchFoodLayout;
 import view.customizeView.search_foods_view.SearchFoodView;
 
 import javax.swing.*;
@@ -7,14 +10,17 @@ import javax.swing.*;
 public class CustomizeWindow extends JPanel{
     public CustomizeWindow(){
         SearchFoodView sfp = new SearchFoodView();
-//        AddFoodsPanel afp = new AddFoodsPanel();
+        SearchFoodLayout sflay = new SearchFoodLayout();
+        sfp.setLayout(sflay);
+        AddFoodsView afp = new AddFoodsView();
+        AddFoodsLayout afl = new AddFoodsLayout();
+        afp.setLayout(afl);
 //        BackPanel bp = new BackPanel();
         this.add(sfp);
-//        this.add(afp);
+        this.add(afp);
+
 //        this.add(bp);
-        this.setLayout(null);
-
         this.updateUI();
-
+        this.setVisible(true);
     }
 }
