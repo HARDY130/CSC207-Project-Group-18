bc6c60bbb54e2154e8c9fa08a5fbee14b7690b4a
HARDY130 <haohardy.hu@mail.utoronto.ca>
2024-11-24 12:29:19 -0500

    All functions of view 6 finished.

--- src/view/customizeView/CustomizeWindow.java
+++ src/view/customizeView/CustomizeWindow.java
@@ -1,5 +1,6 @@
 package view.customizeView;
 
+import interface_adapter.customize.search_foods.SearchFoodViewModel;
 import view.customizeView.add_foods_view.AddFoodsLayout;
 import view.customizeView.add_foods_view.AddFoodsView;
 import view.customizeView.search_foods_view.SearchFoodLayout;
@@ -8,11 +9,13 @@ import view.customizeView.search_foods_view.SearchFoodView;
 import javax.swing.*;
 
 public class CustomizeWindow extends JPanel{
-    public CustomizeWindow(){
-        SearchFoodView sfp = new SearchFoodView();
+    public CustomizeWindow(?controller){
+        SearchFoodViewModel searchFoodViewModel = new SearchFoodViewModel();
+        SearchFoodView sfp = new SearchFoodView(searchFoodViewModel);
         SearchFoodLayout sflay = new SearchFoodLayout();
         sfp.setLayout(sflay);
-        AddFoodsView afp = new AddFoodsView();
+        AddFoodsView afp = new AddFoodsView(?);
+        searchFoodViewModel.addPropertyChangeListener(afp);
         AddFoodsLayout afl = new AddFoodsLayout();
         afp.setLayout(afl);
 //        BackPanel bp = new BackPanel();
