6263b208f10ce1480828c2dbda791063dfcc5909
HARDY130 <haohardy.hu@mail.utoronto.ca>
2024-12-01 01:10:52 -0500

    New unit test

--- src/interface_adapter/customize/search_foods/SearchFoodViewModel.java
+++ src/interface_adapter/customize/search_foods/SearchFoodViewModel.java
@@ -2,11 +2,11 @@ package interface_adapter.customize.search_foods;
 
 import entity.Food;
 import interface_adapter.ViewModel;
-import use_case.customize.search_food.Constant;
 
 public class SearchFoodViewModel extends ViewModel<SearchFoodState> {
     public SearchFoodViewModel() {
         super("Search Food");
-        setState(new SearchFoodState(new Food[Constant.LENGTHOFFOODS]));
+        final int LENGTHOFFOODS = 3;
+        setState(new SearchFoodState(new Food[LENGTHOFFOODS]));
     }
 }
