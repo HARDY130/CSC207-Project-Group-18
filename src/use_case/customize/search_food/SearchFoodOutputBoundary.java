a9b2f3acd96a40b55854269f818ddf5affff9406
HARDY130 <haohardy.hu@mail.utoronto.ca>
2024-12-01 19:43:14 -0500

    Customize user case

--- src/use_case/customize/search_food/SearchFoodOutputBoundary.java
+++
@@ -1,8 +0,0 @@
-package use_case.customize.search_food;
-
-public interface SearchFoodOutputBoundary {
-
-    void prepareSuccessView(SearchFoodOutputData outputData);
-
-    void prepareFailView(String errorMessage);
-}
