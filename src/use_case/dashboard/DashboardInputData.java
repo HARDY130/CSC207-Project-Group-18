57772ed3a28f639a0888048ba43ae06e3265f285
JeromeJinzh <jinzihan.jin@mail.utoronto.ca>
2024-12-04 07:15:40 +0800

    Delete instance and parameters which is redundant for the class

--- src/use_case/dashboard/DashboardInputData.java
+++ src/use_case/dashboard/DashboardInputData.java
@@ -6,31 +6,10 @@ import java.util.Set;
 
 public class DashboardInputData {
     private final String username;
-    private final LocalDate birthDate;
-    private final String gender;
-    private final int weight;
-    private final int height;
-    private final double activityMultiplier;
-    private final Set<Allergy> allergies;
 
-    public DashboardInputData(String username, LocalDate birthDate, String gender,
-                              int weight, int height, double activityMultiplier,
-                              Set<Allergy> allergies) {
+    public DashboardInputData(String username) {
         this.username = username;
-        this.birthDate = birthDate;
-        this.gender = gender;
-        this.weight = weight;
-        this.height = height;
-        this.activityMultiplier = activityMultiplier;
-        this.allergies = allergies;
     }
 
-    // Getters
     public String getUsername() { return username; }
-    public LocalDate getBirthDate() { return birthDate; }
-    public String getGender() { return gender; }
-    public int getWeight() { return weight; }
-    public int getHeight() { return height; }
-    public double getActivityMultiplier() { return activityMultiplier; }
-    public Set<Allergy> getAllergies() { return allergies; }
 }
