9c90da1412ad228446547076f052cba27a72e0c0
jeffreyshen0 <jeffrey.shen@mail.utoronto>
2024-12-03 20:41:38 -0500

    fixed shadow account?

--- src/use_case/mealplanner/MealPlannerOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package use_case.mealplanner;
-
-public interface MealPlannerOutputBoundary {
-    void prepareSuccessView(MealPlannerOutputData outputData);
-    void prepareFailView(String error);
-    void backToDashboard();
-}
