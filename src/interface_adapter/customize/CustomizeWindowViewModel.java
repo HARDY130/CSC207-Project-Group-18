58f01a8983f389a01b8d828cdee6630483447b75
jeffreyyshen0 <jeffrey.shen@mail.utoronto>
2024-12-03 20:28:32 -0500

    fixed some imports, and some style constraints for you :)

--- src/interface_adapter/customize/CustomizeWindowViewModel.java
+++ src/interface_adapter/customize/CustomizeWindowViewModel.java
@@ -1,6 +1,9 @@
 package interface_adapter.customize;
 
 import interface_adapter.ViewModel;
+/**
+ * The view model for the customize page.
+ */
 
 public class CustomizeWindowViewModel extends ViewModel<String> {
 
